35bebe1415380609550df031d019d05c936582d2
Ash Zarif <muhatashim@google.com>
2025-08-20 16:05:32 -0700

    cleanup

--- flutter_plugin_test2/example/android/app/src/main/kotlin/com/example/flutter_plugin_test_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_plugin_test_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_plugin_test2/lib/flutter_plugin_test.dart
+++
@@ -1,8 +0,0 @@
-
-import 'flutter_plugin_test_platform_interface.dart';
-
-class FlutterPluginTest {
-  Future<String?> getPlatformVersion() {
-    return FlutterPluginTestPlatform.instance.getPlatformVersion();
-  }
-}
